c57746a63558b25752f3ed17e844741d623760b1
Sergio Vela <102852790+SergioVelaAlfajarin@users.noreply.github.com>
2022-06-09 00:19:01 +0200

    Pequeños ajustes finales

--- src/main/java/pro/calc/main/Calculadora.java
+++ src/main/java/pro/calc/main/Calculadora.java
@@ -4,6 +4,6 @@ import pro.calc.vistas.VistaPrincipal;
 
 public class Calculadora {
     public static void main(String[] args) {
-        new VistaPrincipal();
+        var v = new VistaPrincipal();
     }
 }
