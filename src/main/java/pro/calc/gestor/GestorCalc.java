defe4609dc44d15f82243f79d47923b7e9a0e3ae
Sergio Vela <102852790+SergioVelaAlfajarin@users.noreply.github.com>
2022-09-01 11:23:22 +0200

    empezada nueva logica calculo

--- src/main/java/pro/calc/gestor/GestorCalc.java
+++ src/main/java/pro/calc/gestor/GestorCalc.java
@@ -3,19 +3,21 @@ package pro.calc.gestor;
 import java.util.LinkedList;
 import java.util.ListIterator;
 import pro.calc.exception.CalcException;
+import pro.calc.modelo.Operacion;
 
-public abstract class GestorCalc {//TODO CAMBIAR ESTE METODO ENTERO
+public abstract class GestorCalc {
 
     public static String calcularOperacion(String operacion) {
-        LinkedList<Character> list=  separarOperacion(operacion);  
-        String resultado = calcula(list);
-        return resultado;
+        LinkedList<Character> list = separarOperacion(operacion);
+        Operacion resultado = calcula(list);
+        return null;
     }
 
     /**
      * separa la operacion
+     *
      * @param operacion
-     * @return 
+     * @return
      */
     private static LinkedList<Character> separarOperacion(String operacion) {
         int oplength = operacion.length();
@@ -26,24 +28,60 @@ public abstract class GestorCalc {//TODO CAMBIAR ESTE METODO ENTERO
         print(list);
         return list;
     }
-    
-    private static String calcula(LinkedList<Character> list) {
-       
-        //devolvera un objeto tipo operacion.
-        //este objeto se usara para guardar en ficheros y bbdd
-        //eliminar esta clase y usar por completo la clase operacion.
-        
-        //eliminar parentesis de la operacion
-        //usando la recursividad.
-        
+
+    private static Operacion calcula(LinkedList<Character> list) {
+        int parentesisA = list.lastIndexOf('(');
+
+        if (parentesisA >= 0) {//parentesis found
+            int parentesisB = getSiguienteParentesisCierre(parentesisA, list);
+            //parentesis found - any error throwed
+            LinkedList<Character> sublist = (LinkedList<Character>) list.subList(parentesisA, parentesisB);
+            
+            Operacion op = calcula(sublist);
+            
+            eliminaPos(list, parentesisA, parentesisB);
+            
+            //agregar resultado a la lista original
+        }
         //si no hay parentesis recorrera la lista calculando y resolviendo
         
+        for (int i = 0; i < list.size(); i++) {
+            char c = list.get(i);
+            if(c == '*' || c == '/'){
+                //signo found
+                //recorrer hacia delante y hacia atras,
+                //obteniendo todo que sea un numero o una coma/punto
+                //si no lo es terminara de recorrerlo y se lo agregara a un obj tipo operacion
+            }
+        }
+        
+        
+
+
+       
         return null;
     }
-    
+
+    private static void eliminaPos(LinkedList<Character> list, int parentesisA, int parentesisB) {
+        for (int i = 0; i < list.size(); i++) {
+            if(i >= parentesisA || i <= parentesisB){
+                list.remove(i);
+            }
+        }
+    }
+
     private static void print(LinkedList<Character> l) {
         for (char c : l) {
             System.out.println(c);
         }
+    }
+
+    private static int getSiguienteParentesisCierre(int parentesisA, LinkedList<Character> list) {
+        for (int i = parentesisA; i < list.size(); i++) {
+            if(list.get(i) == ')'){
+                return i;
+            }
+        }
+        throw new CalcException("Operacion mal formada");
     }
 }
---
+++ src/main/java/pro/calc/modelo/Operacion.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pro.calc.modelo;
+
+/**
+ *
+ * @author sergi
+ */
+public class Operacion {
+
+    
+}
